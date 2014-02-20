f143f4e7a63ca03bcd016ef7e9dc282c45ea390c
Anuraag <Anuraag@Anuraag-Mac.local>
2014-02-20 17:31:16 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   match '/signup', to: 'users#new', via: 'get'
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
@@ -7,6 +12,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
